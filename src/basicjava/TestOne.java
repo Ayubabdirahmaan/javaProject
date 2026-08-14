5dbf6f5ad78226cd3b51f373484603ac288124d0
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-08-14 19:32:28 +0300

    Added

---
+++ src/basicjava/TestOne.java
@@ -0,0 +1,10 @@
+package basicjava;
+public class TestOne {
+    public TestOne() {
+        System.out.println("hello wolrd welcome");
+        int num1 = 90;
+        int num2 = 80;
+        int sum = num1+num2;
+        System.out.println(sum);
+    }
+}
